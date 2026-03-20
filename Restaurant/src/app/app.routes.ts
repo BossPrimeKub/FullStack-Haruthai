68f3f6cd3d8a571577de403dad682e92df4437a7
BossPrimeKub <thanachotprime@gmail.com>
2026-03-20 20:41:56 +0700

    boss

--- Restaurant/src/app/app.routes.ts
+++ Restaurant/src/app/app.routes.ts
@@ -4,3 +4,4 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' }
 ];
+// 
--- Restaurant/src/app/pages/home/home.component.ts
+++ Restaurant/src/app/pages/home/home.component.ts
@@ -39,3 +39,4 @@ export class HomeComponent implements OnInit, OnDestroy {
     }, 5000);
   }
 }
+// 
